9662b471a622f24c6c0cd6f8746dfe7ca50f1feb
tbconrad <tbconrad@gmail.com>
2016-11-11 01:14:02 -0500

    Add files via upload

---
+++ WidgetTestService/Global.asax.cs
@@ -0,0 +1,29 @@
+﻿#pragma warning disable 1591
+using System.Data.Entity;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using WidgetTestService.DAL;
+
+namespace WidgetTestService
+{
+    public class WebApiApplication : HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<WidgetContext>());
+            using (var context = new WidgetContext())
+            {
+                context.Database.Initialize(force: true);
+            }
+        }
+    }
+}
